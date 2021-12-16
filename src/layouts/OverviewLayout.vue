b27cd96c688bfc55094564cda5a0d2060fb3bf18
julianegoe <goersch.juliane@dpa.com>
2021-12-16 20:15:08 +0100

    styled overview

---
+++ src/layouts/OverviewLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="overview">
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'OverviewLayout',
+}
+</script>
+<style scoped>
+
+.overview {
+  position: absolute;
+  top: 2rem;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  box-sizing: border-box;
+  background: #f1f1f1;
+
+}
+
+</style>
--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,20 +1,51 @@
 <template>
-    <div class="flex-container">
-        <div v-for="user, index in users" :key="index">
-            <RouterLink v-if="user != currentUser" :to="{ name: 'UserTree', params: { id: user || 'no_ID' } }">
-                {{user}}
-            </RouterLink>
+<OverviewLayout>
+    <div class="overview-container"> 
+        <div class="rules-container">
+            <div class="rules-box">
+                <h1>Bier Turnier 2022 - Regeln</h1>
+                <p>
+                    <ol>
+                        <li>In der Beer Challenge 2022 soll jeder Teilnehmer die Möglichkeit haben, das, seiner Meinung nach, am besten schmeckenste Bier zu finden.
+                            Der Zeitraum dafür ist das komplette Jahr 2022.
+                        </li>
+                        <li>Über die Website verfranzt.com/beer kann jeder Teilnehmer seinen eigenen Turnierbaum mit allen ausgewählten Biermarken pflegen. </li>
+                        <li>Die Website wird von einem Login Bereich aus datenschutzrechtlichen Gründen geschützt.</li>
+                        <li>Jeder Teilnehmer kann alle Turnierbäume einsehen. </li>
+                        <li>Bei einer Stimmenabgabe treten immer zwei Biermarken gegeneinander an. </li>
+                        <li>Zugelasene Biersorten sind: Pils, Helles, Kölsch, Schwarzbier, Kellerbier, Bockbier, Export, hefeweizen, Stout</li>
+                        <li>Nicht zugelassene Biersorten sind: Craftbiere aller Art, Biermischgetränke wie Radler, Diesel etc., ALKOHOLFREIE Biere, Malzbiere, Belgische Biersorten, Cider</li>
+                        <li>Die Verkostung sollte nach Möglichkeit "blind" erfolgen. Das heißt, der Teilnehmer nutzt zur Verköstigung zwei Gläser und lässt diese von einer zweiten Person austauschen.</li>
+                        <li>Bis 23.12.2021 entsteht die verbindliche Auswahl aus 32 Biermarken, die für alle Teilnehmer zur Wahl stehen. </li>
+                    </ol>
+                </p>
+            </div>
+        </div>
+        <div>
+        <div class="user-container">
+            <h2>Teilnehmerinnen und Teilnehmer</h2>
+            <template v-for="user, index in users" :key="index">
+                <div @click="$router.push({ name: 'UserTree', params: { id: user || 'no_ID' } })" class="user-box" v-if="user != currentUser" >
+                    {{user}}
+                </div>
+            </template>
+            <!-- <div class="user-box">Ronald</div>
+            <div class="user-box">Daniel</div>
+            <div class="user-box">Janna</div> -->
+        </div>
         </div>
     </div>
+ </OverviewLayout>
 </template>
 
 <script>
 import { db, auth } from '@/firebase';
 import { collection, getDocs } from "firebase/firestore";
+import OverviewLayout from '@/layouts/OverviewLayout.vue';
 
     export default {
         name: 'Overview',
-        components: {
+        components: { OverviewLayout,
         },
         data() {
             return {
@@ -44,10 +75,46 @@ import { collection, getDocs } from "firebase/firestore";
 
 <style scoped>
 
-.flex-container {
+.overview-container {
+    display: grid;
+    grid-template-columns: 60% 40%;
+}
+
+.rules-container {
     display: flex;
-    flex-direction: row;
-    justify-content: space-around;
+    flex-direction: column;
+    justify-content: flex-start;
+    padding: 2rem;
+}
+
+.rules-box {
+    padding: 0 1rem 0 1rem ;
+    background: white;
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+}
+
+h1, p {
+    text-align: left;
+}
+
+.user-container {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 0.5rem;
+    padding: 2rem 2rem 2rem 0;
+}
+
+h2 {
+    grid-column: 1 / span 2;
+    text-align: left;
+}
+
+.user-box {
+    padding: 1rem;
+    background: white;
+    box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+    cursor: pointer;
+
 }
 
 </style>
