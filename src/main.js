6e06d751b3002e2b356f8f194af012ab0e6a5a65
julianegoe <goersch.juliane@dpa.com>
2021-12-18 20:45:32 +0100

    fixed login state error

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
+import { auth } from './firebase';
 
-createApp(App).use(router)
-.use(router)
-.mount('#app')
+let app;
+
+auth.onAuthStateChanged(() => {
+    if (!app) {
+        app = createApp(App)
+            .use(router)
+            .mount('#app')
+    }
+})
